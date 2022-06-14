c3e26f4494731f13fe3873a0e152bbb7d49bd3b3
Sumano <sumano15032002@gmail.com>
2022-06-14 14:26:16 +0800

    gnav depedencies

--- lib/siginin.dart
+++ lib/siginin.dart
@@ -33,6 +33,25 @@ class _SignInScreenState extends State<SignInScreen> {
     return numericRegex.hasMatch(string);
   }
 
+  showLoaderDialog(BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      content: new Row(
+        children: [
+          CircularProgressIndicator(),
+          Container(
+              margin: EdgeInsets.only(left: 7), child: Text("Loading...")),
+        ],
+      ),
+    );
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
   void _signUp(
       String _email, String _password, String _nama, String _noTelp) async {
     try {
@@ -223,6 +242,7 @@ class _SignInScreenState extends State<SignInScreen> {
                       ),
                     ),
                     onPressed: () {
+                      showLoaderDialog(context);
                       if (NamaController.text.isEmpty) {
                         showDialog(
                           context: context,
