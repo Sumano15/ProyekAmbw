0e126decd3ec79d7f130a985d77474c500ad169b
SastraH78 <sastrahimawan123@gmail.com>
2022-06-21 00:32:35 +0700

    ya

--- lib/admin/buatMenu.dart
+++ lib/admin/buatMenu.dart
@@ -1,7 +1,217 @@
+// import 'dart:io';
+
+// import 'package:dropdown_button2/dropdown_button2.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// void main() async {
+//   await Firebase.initializeApp();
+// }
+
+// class BuatMenu extends StatefulWidget {
+//   const BuatMenu({Key? key}) : super(key: key);
+
+//   @override
+//   State<BuatMenu> createState() => _BuatMenuState();
+// }
+
+// class _BuatMenuState extends State<BuatMenu> {
+//   FilePickerResult? result;
+//   String? fileName;
+//   PlatformFile? pickedfile;
+//   bool isLoading = false;
+//   File? fileToDisplay;
+
+//   void pickFile() async {
+//     try {
+//       setState(() {
+//         isLoading = true;
+//       });
+
+//       result = await FilePicker.platform.pickFiles(
+//         type: FileType.custom,
+//         allowedExtensions: ['jpg', 'png', 'jpeg'],
+//         allowMultiple: false,
+//       );
+
+//       if (result != null) {
+//         fileName = result!.files.first.name;
+//         pickedfile = result!.files.first;
+//         fileToDisplay = File(pickedfile!.path.toString());
+//       }
+//       setState(() {
+//         isLoading = false;
+//       });
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+
+//   Future uploadFotoToStorage() async {
+//     final path = "menu/$fileName";
+//     // final file = File(pickedfile!.path.toString());
+//     final file = File(pickedfile!.path!.toString());
+
+//     final ref = FirebaseStorage.instance.ref().child(path);
+//     ref.putFile(file);
+//   }
+
+//   final items = ['Makanan', 'Minuman'];
+//   String? value;
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Container(
+//         padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+//         child: Column(
+//           children: [
+//             SizedBox(
+//               height: 20,
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 if (fileToDisplay == null)
+//                   Align(
+//                     alignment: Alignment.center,
+//                     child: CircleAvatar(
+//                       radius: 100,
+//                       backgroundColor: Colors.white,
+//                       child: ClipOval(
+//                         child: SizedBox(
+//                           width: 200,
+//                           height: 200,
+//                           child: Image.network(
+//                             "https://i0.wp.com/www.charitycomms.org.uk/wp-content/uploads/2019/02/placeholder-image-square.jpg?ssl=1",
+//                             fit: BoxFit.fill,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 if (fileToDisplay != null)
+//                   Align(
+//                     alignment: Alignment.center,
+//                     child: CircleAvatar(
+//                       radius: 100,
+//                       backgroundColor: Colors.white,
+//                       child: ClipOval(
+//                         child: SizedBox(
+//                           width: 200,
+//                           height: 200,
+//                           child: Image.file(
+//                             fileToDisplay!,
+//                             fit: BoxFit.fill,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 Padding(
+//                   padding: EdgeInsets.only(top: 60.0),
+//                   child: IconButton(
+//                     icon: Icon(
+//                       Icons.camera_alt,
+//                       size: 30,
+//                     ),
+//                     onPressed: () {
+//                       pickFile();
+//                     },
+//                   ),
+//                 )
+//               ],
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             DropdownButton<String>(
+//               borderRadius: BorderRadius.circular(10),
+//               itemHeight: 50,
+//               hint: Text("Kategori"),
+//               value: value,
+//               isExpanded: true,
+//               items: items.map(buildMenuItem).toList(),
+//               onChanged: (value) {
+//                 setState(() {
+//                   this.value = value;
+//                 });
+//               },
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             TextField(
+//               decoration: InputDecoration(
+//                 labelText: 'Nama Menu',
+//                 labelStyle: TextStyle(fontSize: 20),
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             TextField(
+//               decoration: InputDecoration(
+//                 labelText: 'Harga',
+//                 labelStyle: TextStyle(fontSize: 20),
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 20,
+//             ),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 primary: Color.fromRGBO(240, 188, 98, 1),
+//                 fixedSize: const Size(200, 60),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(25),
+//                 ),
+//               ),
+//               onPressed: () {
+//                 uploadFotoToStorage();
+//               },
+//               child: Text(
+//                 'Add Menu',
+//                 style: TextStyle(
+//                   fontSize: 25,
+//                   color: Colors.black,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   DropdownMenuItem<String> buildMenuItem(String item) {
+//     return DropdownMenuItem(
+//       value: item,
+//       child: Text(item),
+//     );
+//   }
+// }
+
 import 'dart:io';
 
+import 'package:ambwproyek/dataclass.dart';
+import 'package:ambwproyek/dbservices.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +219,10 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
+
+void main() async {
+  await Firebase.initializeApp();
+}
 
 class BuatMenu extends StatefulWidget {
   const BuatMenu({Key? key}) : super(key: key);
@@ -23,6 +237,9 @@ class _BuatMenuState extends State<BuatMenu> {
   PlatformFile? pickedfile;
   bool isLoading = false;
   File? fileToDisplay;
+
+  final _namaMakananController = TextEditingController();
+  final _hargaController = TextEditingController();
 
   void pickFile() async {
     try {
@@ -41,7 +258,6 @@ class _BuatMenuState extends State<BuatMenu> {
         pickedfile = result!.files.first;
         fileToDisplay = File(pickedfile!.path.toString());
       }
-
       setState(() {
         isLoading = false;
       });
@@ -50,9 +266,13 @@ class _BuatMenuState extends State<BuatMenu> {
     }
   }
 
+  String URL = "";
+
   Future uploadFotoToStorage() async {
     final path = "menu/$fileName";
-    final file = File(pickedfile!.path.toString());
+    URL = path;
+    // final file = File(pickedfile!.path.toString());
+    final file = File(pickedfile!.path!.toString());
 
     final ref = FirebaseStorage.instance.ref().child(path);
     ref.putFile(file);
@@ -143,6 +363,7 @@ class _BuatMenuState extends State<BuatMenu> {
               height: 20,
             ),
             TextField(
+              controller: _namaMakananController,
               decoration: InputDecoration(
                 labelText: 'Nama Menu',
                 labelStyle: TextStyle(fontSize: 20),
@@ -155,6 +376,7 @@ class _BuatMenuState extends State<BuatMenu> {
               height: 20,
             ),
             TextField(
+              controller: _hargaController,
               decoration: InputDecoration(
                 labelText: 'Harga',
                 labelStyle: TextStyle(fontSize: 20),
@@ -176,6 +398,7 @@ class _BuatMenuState extends State<BuatMenu> {
               ),
               onPressed: () {
                 uploadFotoToStorage();
+                final dtBaru = menu(kategori: value.toString(), nama: _namaMakananController.toString(), harga: _hargaController.toString(), gambar: URL);
               },
               child: Text(
                 'Add Menu',
@@ -199,3 +422,4 @@ class _BuatMenuState extends State<BuatMenu> {
     );
   }
 }
+
--- lib/dbservices.dart
+++ lib/dbservices.dart
@@ -1,8 +1,25 @@
+import 'package:ambwproyek/dataclass.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-CollectionReference tblCatatan =
-    FirebaseFirestore.instance.collection("tabelCatatan");
+CollectionReference tblMenu = FirebaseFirestore.instance.collection("menu");
 
-class Databse {
+class Database {
+  static Stream<QuerySnapshot> getData(String namaMenu) {
+    if (namaMenu == "") {
+      return tblMenu.snapshots();
+    } else {
+      return tblMenu
+          .orderBy("nama")
+          .snapshots();
+    }
+  }
+
+  static Future<void> tambahData({required menu itemMenu}) async {
+    DocumentReference docRef = tblMenu.doc(itemMenu.nama);
+    await docRef
+        .set(itemMenu.toJson())
+        .whenComplete(() => print("Data berhasil diTambah"))
+        .catchError((e) => print(e));
+  }
 
 }
