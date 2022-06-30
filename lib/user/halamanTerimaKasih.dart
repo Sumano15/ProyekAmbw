a474ed6aa79c3ba2d169510d2ec45a309cde3cca
Sumano <sumano15032002@gmail.com>
2022-07-01 02:02:50 +0700

    user selesai

--- lib/user/halamanTerimaKasih.dart
+++ lib/user/halamanTerimaKasih.dart
@@ -19,62 +19,67 @@ class _HalTerimaKasihState extends State<HalTerimaKasih> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-          children: [
-            SizedBox(
-              height: 100,
+        children: [
+          SizedBox(
+            height: 100,
+          ),
+          Center(
+            child: Text(
+              'Terima Kasih telah melakukan pembayaran',
+              style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15),
             ),
-            Center(
-              child: Text(
-                'Terima Kasih telah melakukan pembayaran',
-                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 16),
-              ),
-            ),
-            Expanded(
-              child: FutureBuilder<QuerySnapshot>(
-                future: dataTransaksi.getDataMenu(widget.idPesanan),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (snapshot.hasData || snapshot.data != null) {
-                    return ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        DocumentSnapshot doc = snapshot.data!.docs[index];
-                        return Container(
-                          child: Column(
-                            children: [
-                              ListTile(
-                                title: Text(doc.id),
-                                subtitle: Text(doc['Harga'].toString()),
-                                trailing: Text(doc['Status'].toString()),
-                                leading: ConstrainedBox(
-                                  constraints: BoxConstraints(
-                                    minWidth: 100,
-                                    minHeight: 100,
-                                    maxWidth: 200,
-                                    maxHeight: 200,
-                                  ),
-                                  child: Image.network(
-                                    doc['Gambar'],
-                                    fit: BoxFit.cover,
-                                  ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text('Pesanan Anda:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Expanded(
+            child: FutureBuilder<QuerySnapshot>(
+              future: dataTransaksi.getDataMenu(widget.idPesanan),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData || snapshot.data != null) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot doc = snapshot.data!.docs[index];
+                      return Container(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text(doc.id),
+                              subtitle: Text(doc['Harga'].toString()),
+                              trailing: Text(doc['Status'].toString()),
+                              leading: ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  minHeight: 100,
+                                  maxWidth: 200,
+                                  maxHeight: 200,
+                                ),
+                                child: Image.network(
+                                  doc['Gambar'],
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                    ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   );
-                },
-              ),
-            )
-          ],
+                }
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
